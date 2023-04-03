fe8eee6b9ff4336971d5e1c7e6cc652c3d13c81f
jeanPROangeles <jean.angeles@utec.edu.pe>
2023-04-03 15:50:01 -0500

    Update Criba.py

--- open-bootcamp/curso-intensivo-desde0/ejercicio3-cribaEratostenes/Criba.py
+++ open-bootcamp/curso-intensivo-desde0/ejercicio3-cribaEratostenes/Criba.py
@@ -3,7 +3,7 @@ def find_primes(N):
     is_prime[0] = False
     is_prime[1] = False
 
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(2, int(N**0.5) + 1):
         if is_prime[i]:
             for j in range(i*i, N+1, i):
                 is_prime[j] = False
@@ -11,3 +11,6 @@ def find_primes(N):
     primes = [num for num in range(2, N+1) if is_prime[num]]
 
     return primes
+
+
+  
